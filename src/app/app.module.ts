d590bb0aa0a7fad79ed5fd2f0537fff49dc07256
Nantanatorn Suetrong <nantanatorn.s@ku.th>
2025-09-12 12:20:07 +0700

    app-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutusComponent } from './Pages/aboutus/aboutus.component';
+import { ShopComponent } from './Pages/shop/shop.component';
+import { ServiceComponent } from './Pages/service/service.component';
+import { ProductDetailsComponent } from './Pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    ShopComponent,
+    ServiceComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
