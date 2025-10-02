65e9a08c785fc97c5b5484c1017f88d83c20427f
Nantanatorn Suetrong <nantanatorn.s@ku.th>
2025-10-02 20:57:12 +0700

    route fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './Pages/home/home.component';
 import { AboutusComponent } from './Pages/aboutus/aboutus.component';
 
 const routes: Routes = [
-  {path: '',component: HomeComponent},
+  {path: '',component: HomeComponent, pathMatch: 'full' },
   {path:'about',component: AboutusComponent},
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
